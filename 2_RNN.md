3e8ca3f92ad2c4b4204362003effa2f8f183ed2e
pttsrd <ruslan2kiev@gmail.com>
2024-03-18 23:47:40 +0800

    add log

--- 2_RNN.md
+++ 2_RNN.md
@@ -1,3 +1,9 @@
 # [2_RNN.py](./2_RNN.py)
 
-## 
+## 记录
+
+用RNN做图像分类总有种不太对劲的感觉，因为不知道RNN所要的`序列`该怎么取。
+看了Github上这个数据集里的图片，一开始设想按行/列为时间单位顺序输入是一种方法，但结果训练出的正确率不如像素一个一个来。虽然结果上正确率也没有上90%。
+![Fashion-Mnist](https://github.com/zalandoresearch/fashion-mnist/blob/master/doc/img/fashion-mnist-sprite.png)
+
+总之，RNN跟基础神经网络的区别就是可以处理有某种顺序的序列数据，吃一口，消化一下，再吃再消化，而不是等吃完才能消化。
--- 3_Attention.py
+++ 3_Attention.py
@@ -9,7 +9,7 @@ def transpose_qkv(X, n_heads):
 
 def transback_qkv(X, n_heads):
     X = X.reshape(-1, n_heads, X.shape[1], X.shape[2]).transpose(1,2)
-    return X.contiguous().reshape(X.shape[0], X.shape[1], -1)
+    return X.reshape(X.shape[0], X.shape[1], -1)
 
 
 class MultiHeadAttention(nn.Module):
@@ -38,7 +38,7 @@ class MultiHeadAttention(nn.Module):
         K = transpose_qkv(K, self.n_heads)
         V = transpose_qkv(V, self.n_heads)
 
-        # 计算注意力权重
+        # 计算注意力权重： Q和K的特征维度相同，可采用缩放点积法
         A = torch.matmul(Q, K.transpose(-2, -1)) / torch.sqrt(torch.tensor(self.n_hidden // self.n_heads))
         if mask is not None:
             A = A.masked_fill(mask == 0, -1e9)
@@ -58,12 +58,14 @@ class MultiHeadAttention(nn.Module):
 
 # 创建随机矩阵
 batch_size = 2
-n_qkv = 10
+n_q = 10
+n_kv = 20
 n_hidden = 32
 n_heads = 8
 
-Q = torch.randn(batch_size, n_qkv, n_hidden)
-K = V = torch.randn(batch_size, n_qkv*2, n_hidden)
+Q = torch.randn(batch_size, n_q, n_hidden)
+K = torch.randn(batch_size, n_kv, n_hidden)
+V = torch.randn(batch_size, n_kv, n_hidden)
 
 # 创建多头注意力模型
 attention = MultiHeadAttention(n_hidden, n_heads)
